e8b7b1bfa9eac49471a07892f7ae06a3dfb4354f
belloemmanuel <belloemmanuel02@gmail.com>
2024-02-29 08:05:27 +0100

    usestate array

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0
+  const increaseCount =()=>{
+    count = count + 1;
+    console.log(count)
+  }
+  return (
+    <>
+    <h>This is our {count}</h>
+    <button type='button' onClick={increaseCount} className="btn">Increase</button>
+    </>
+  )
 };
 
 export default ErrorExample;
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,26 @@
+import React, { useState } from "react";
+import {data} from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data)
+
+  const removeItem = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+
+  return (
+  <>
+  {people.map((person)=>{
+    const {id, name} = person
+    return <div key={id}>
+      <h4>{name}</h4>
+      <button type="button" className="btn" onClick={()=>removeItem(id)}>remove</button>
+    </div>
+  })}
+  <button type="button" className="btn" style={{color:'white', backgroundColor:'red'}} onClick={()=>setPeople([])}>Clear all</button>
+  <button type="button" className="btn" style={{color:'white', backgroundColor:'green'}} onClick={()=>setPeople(data)}>Reset List</button>
+  </>
+  )
 };
 
 export default UseStateArray;
