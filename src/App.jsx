d124996acf4268f0f9615be1888e3a3addf6654a
belloemmanuel <belloemmanuel02@gmail.com>
2024-02-29 08:26:54 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Starter from './tutorial/01-useState/starter/03-useState-array'
+import Starter from './tutorial/01-useState/starter/04-useState-object'
 function App() {
   return (
     <div className='container'>
