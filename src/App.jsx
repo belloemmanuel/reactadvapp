87a323cd2f490d0569b2431aefd2e534b36b699a
belloemmanuel <belloemmanuel02@gmail.com>
2024-03-05 16:06:01 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Starter from './tutorial/01-useState/starter/05-useState-gotcha'
+import Starter from './tutorial/02-useEffect/starter/04-fetch-data'
 function App() {
   return (
     <div className='container'>
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try {
+        const response = await fetch(url);
+      const users = await response.json()
+      console.log(users);
+      } catch (error) {
+        console.log(error)
+      }
+      
+    };
+    fetchData()
+  },[]);
+
+  return (
+  <section>
+    <h3>github users</h3>
+    <ul className="users">
+   {users.map((user)=>{
+    return <li>item</li>
+   })}
+    </ul>
+  </section>
+    )
 };
 export default FetchData;
